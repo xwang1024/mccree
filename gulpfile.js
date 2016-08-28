0e5559b11d88dddb8eff4733da3e6d482ec28b7a
XWang1024 <xwang1024@126.com>
2016-08-29 00:40:33 +0800

    less compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,30 @@
-var vendor = require('./vendor.json');
+var resource = require('./resource.json');
 
 var gulp     = require('gulp'),
     $        = require('gulp-load-plugins')(),
     gulpsync = $.sync(gulp);
 
-var isProduction = true;
+var isProduction = false;
 
 // compile marko to marko.js 
 gulp.task('marko', function() {
   return gulp.src('app/templates/*.marko')
-             .pipe($.markoc({preserveWhitespace: true})
-             .on('error', handleError))
-             .pipe(gulp.dest('lib/views'))
+    .pipe(pLog('Compiling marko templates...'))
+    .pipe($.markoc({preserveWhitespace: true})
+    .on('error', handleError))
+    .pipe(gulp.dest('lib/views'))
+    .pipe(pLog('Marko templates all compiled!'));
 });
 
 // compile script
 gulp.task('script', function() {
-  if(!vendor.script) {
-    return log('There is no script attribute in vendor.json');
+  if(!resource.script) {
+    return log('There is no script attribute in resource.json');
   }
-  for(var k in vendor.script) {
+  for(var k in resource.script) {
     ((k) => {
-      log('Compiling script ' + k + '...');
-      gulp.src(vendor.script[k])
+      gulp.src(resource.script[k])
+          .pipe($.print(()=>($.util.colors.blue( 'Compiling script ' + k + '...' ))))
           .pipe($.eslint({
             rules: { 'strict': 2 },
             globals: [ 'jQuery', '$' ],
@@ -38,22 +40,46 @@ gulp.task('script', function() {
           .pipe($.if( isProduction, $.uglify() ))
           .pipe($.concat(k))
           .pipe(gulp.dest('public/js/'))
-          .pipe($.print(()=>($.util.colors.blue( k + " compiled!" ))));
+          .pipe(pLog(k + ' compiled!'));
     })(k);
   };
 });
 
+var cssnanoOpts = {
+  safe: true,
+  discardUnused: false, // no remove @font-face
+  reduceIdents: false // no change on @keyframes names
+}
 // compile style
 gulp.task('style', function() {
-  log('style');
+  if(!resource.style) {
+    return log('There is no style attribute in resource.json');
+  }
+  for(var k in resource.style) {
+    ((k) => {
+      gulp.src(resource.style[k])
+        .pipe($.print(()=>($.util.colors.blue( 'Compiling style ' + k + '...' ))))
+        .pipe($.less())
+        .on("error", handleError)
+        .pipe($.if(isProduction, $.cssnano(cssnanoOpts)))
+        .pipe(gulp.dest('public/css/'))
+        .pipe(pLog(k + ' compiled!'));
+    })(k);
+  }
 });
 
 // copy vendor
 gulp.task('vendor', function() {
-
+  if(!resource.vendor) {
+    return log('There is no vendor attribute in vendor.json');
+  }
+  return gulp.src(resource.vendor)
+    .pipe(pLog('Copying vendors...'))
+    .pipe(gulp.dest('public/vendor'))
+    .pipe(pLog('Vendors all copied!'));
 });
 
-gulp.task('default', gulpsync.sync(['marko', 'script', 'style']), function() {
+gulp.task('default', ['marko', 'script', 'style', 'vendor'], function() {
   log('Finished')
 });
 
@@ -66,4 +92,8 @@ function handleError(err) {
 // log to console using 
 function log(msg) {
   $.util.log( $.util.colors.blue( msg ) );  
+}
+
+function pLog(msg) {
+  return $.print(()=>( $.util.colors.blue( msg )) );
 }
