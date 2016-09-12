ea04891032780b78de17d6c4d359d8a971a7af00
XWang1024 <xwang1024@126.com>
2016-09-12 15:35:28 +0800

    中台数据获取逻辑基本成型

--- index.js
+++ index.js
@@ -5,22 +5,54 @@ require('marko/express');
 require('marko/node-require').install();
 require('marko/hot-reload').enable();
 
+const _          = require('lodash');
 const watchTree  = require("fs-watch-tree").watchTree;
 const path       = require('path');
 const http       = require('http');
 const express    = require('express');
 const logger     = require('morgan');
 const bodyParser = require('body-parser');
+const async      = require('async');
+const request    = require('request');
+
+const phpHost    = 'http://mebox.xin.me';
 
 var app = express();
 
 app.use(express.static('public'));
 app.use(logger('dev'));
 
-app.get('/', (req, res, next) => {
-  let template = require('./lib/views/pages/index.marko');
-  res.marko(template, {});
-});
+const serviceConf = require('./conf/service.json');
+const renderConf = require('./conf/render.json');
+
+for(var url in renderConf) {
+  (function(url, conf) {
+    app.get(url, (req, res, next) => {
+      let template = require(conf.view);
+      // get data from PHP Server
+      var dataKeys = Object.keys(conf.data);
+      var context = {};
+      async.each(dataKeys, (dataKey, callback) => {
+        let service = serviceConf[conf.data[dataKey]];
+        request[service.method]({
+          url: phpHost+service.url,
+          headers: _.omit(req.headers, ['cookie', 'refer']),
+        }, (err, response, body) => {
+          if (!err && response.statusCode == 200) {
+            context[dataKey] = JSON.parse(body);
+            callback();
+          } else {
+            console.log(err);
+          }
+        })
+      }, (err) => {
+        console.log(context);
+        res.marko(template, context);
+      });
+      
+    });
+  })(url, renderConf[url]);
+}
 
 app.server = http.createServer(app);
 app.server.listen(3000, function(){
